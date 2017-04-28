3cbeecb5149b3e2b83770b1542ccf36ec41e36f0
Jay Lee <leejg1313@naver.com>
2017-04-28 22:31:36 +0900

    deleted hypong-dynamicEventLogs.js

--- js/examples/hypong-multipleInstances.js
+++
@@ -1,8 +0,0 @@
-/* 
- * hypong.js version 1.0.0, @license MIT, (c) 2017 Jay Lee.
- * : Multiple Instances Example
- */
-const stage1 = hypong('stage1').run();
-const stage2 = hypong('stage2').run();
-const stage3 = hypong('stage3').run();
-const stage4 = hypong('stage4').run();
